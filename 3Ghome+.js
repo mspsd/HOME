881fa420ee9284b921fd58e9a9129278aab321a9
StasyCat <30519111+StasyCat@users.noreply.github.com>
2017-12-11 17:20:27 +0900

    Delete JS file

--- 3Ghome+.js
+++
@@ -1,8 +0,0 @@
-function ChangeTab(tabname) {
-   // 全部消す
-   document.getElementById('tab1').style.display = 'none';
-   document.getElementById('tab2').style.display = 'none';
-   document.getElementById('tab3').style.display = 'none';
-   // 指定箇所のみ表示
-   document.getElementById(tabname).style.display = 'block';
-}
